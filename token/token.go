b7627ab06b26b6f004600cf9abb5b429badba719
Yuma2000 <t.yuma2618@gmail.com>
2023-09-17 11:20:18 +0900

    true, false, if, else, returnの字句解析に対応

--- token/token.go
+++ token/token.go
@@ -33,6 +33,11 @@ const (
 	// キーワード
 	FUNCTION = "FUNCTION"
 	LET      = "LET"
+	TRUE     = "TRUE"
+	FALSE    = "FALSE"
+	IF       = "IF"
+	ELSE     = "ELSE"
+	RETURN   = "RETURN"
 )
 
 type Token struct {
@@ -43,6 +48,11 @@ type Token struct {
 var keywords = map[string]TokenType{
 	"fn":  FUNCTION,
 	"let": LET,
+	"true":   TRUE,
+	"false":  FALSE,
+	"if":     IF,
+	"else":   ELSE,
+	"return": RETURN,
 }
 
 // LookupIdent checks if the given identifier is a keyword.
