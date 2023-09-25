5d845ba020c168603eb0aba61d9c0f365021291b
Yuma2000 <t.yuma2618@gmail.com>
2023-09-25 17:19:48 +0900

    中置演算の構文解析を追加

--- ast/ast.go
+++ ast/ast.go
@@ -144,3 +144,25 @@ func (pe *PrefixExpression) String() string {
 
 	return out.String()
 }
+
+type InfixExpression struct {
+	Token    token.Token // 演算子トークン，例えば「+」
+	Left     Expression
+	Operator string
+	Right    Expression
+}
+
+func (ie *InfixExpression) expressionNode()      {}
+func (ie *InfixExpression) TokenLiteral() string { return ie.Token.Literal }
+// 文字列をバッファに書き込み，バッファを文字列として返す．
+func (ie *InfixExpression) String() string {
+	var out bytes.Buffer
+
+	out.WriteString("(")
+	out.WriteString(ie.Left.String())
+	out.WriteString(" " + ie.Operator + " ")
+	out.WriteString(ie.Right.String())
+	out.WriteString(")")
+
+	return out.String()
+}
